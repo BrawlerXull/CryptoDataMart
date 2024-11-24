2f9a326d44ea44351b73d8406dc3a443e940ae06
Chinmay <chaudhari.chinmay12345@gmail.com>
2024-11-24 16:38:04 +0530

    Ask gemini working

--- frontend/src/pages/DatasetPage.tsx
+++ frontend/src/pages/DatasetPage.tsx
@@ -9,34 +9,27 @@ import { useDatasetDescription } from "../hooks/useDatasetDescription";
 import { Listing } from "../types/listing";
 import { formatCreationTime } from "../utils/helphers";
 import toast from "react-hot-toast";
+import { FaRobot } from "react-icons/fa";
+import useGeminiQuery from "../hooks/useGeminiQuery";
 
 const DatasetPage = () => {
   const { id } = useParams();
   const { listingData } = useListingContract();
-  const {
-    promptTemplate,
-    loading: promptLoading,
-    error: promptError,
-  } = usePromptTemplate();
+  const { promptTemplate, loading: promptLoading, error: promptError } = usePromptTemplate();
   const [dataset, setDataset] = useState<Listing | null>(null);
-  const {
-    csvData,
-    loading: csvLoading,
-    error: csvError,
-  } = useCsvData(dataset?.previewIpfsLink || "");  
+  const { csvData, loading: csvLoading, error: csvError } = useCsvData(dataset?.previewIpfsLink || "");
+  const { generatedDescription, descriptionLoading } = useDatasetDescription(dataset?.id || 0, promptTemplate || "", csvData);
 
-  const { generatedDescription, descriptionLoading } = useDatasetDescription(
-    dataset?.id || 0,
-    promptTemplate || "",
-    csvData
-  );
+  
+  const { response, loading: queryLoading, askQuery } = useGeminiQuery();
+  const [userQuery, setUserQuery] = useState<string>("");
+  const [, setIsQuerySubmitted] = useState<boolean>(false);
+  const [responses, setResponses] = useState<string[]>([]);  
 
   useEffect(() => {
     if (id) {
       const numericId = parseInt(id, 10);
-      const foundDataset = listingData.find(
-        (listing) => listing.id === numericId
-      );
+      const foundDataset = listingData.find((listing) => listing.id === numericId);
       setDataset(foundDataset || null);
     }
   }, [id, listingData]);
@@ -46,6 +39,35 @@ const DatasetPage = () => {
       toast.error(`Error loading CSV data: ${csvError || "Unknown error"}`);
     }
   }, [csvError]);
+
+  const handleQuerySubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!userQuery.trim()) {
+      toast.error("Please enter a valid query.");
+      return;
+    }
+    setIsQuerySubmitted(true);
+    askQuery(userQuery, csvData);  
+    setUserQuery(""); 
+  };
+
+  
+  useEffect(() => {
+    if (response) {
+      setResponses((prevResponses) => [...prevResponses, response]);  
+    }
+  }, [response]);
+
+  const formatDescription = (description: string) => {
+    const sentences = description.split(".").map((sentence) => sentence.trim()).filter(Boolean);
+    return (
+      <ul className="list-disc pl-5">
+        {sentences.map((sentence, index) => (
+          <li key={index}>{sentence}.</li>
+        ))}
+      </ul>
+    );
+  };
 
   if (promptLoading || descriptionLoading || csvLoading) {
     return (
@@ -57,25 +79,12 @@ const DatasetPage = () => {
   }
 
   if (promptError) {
-    return (
-      <div>Error loading prompt template: {promptError || "Unknown error"}</div>
-    );
+    return <div>Error loading prompt template: {promptError || "Unknown error"}</div>;
   }
 
   if (!dataset) {
     return <div>Dataset not found.</div>;
   }
-
-  const formatDescription = (description: string) => {
-    const sentences = description.split(".").map((sentence) => sentence.trim()).filter(Boolean);
-    return (
-      <ul className="list-disc pl-5">
-        {sentences.map((sentence, index) => (
-          <li key={index}>{sentence}.</li> 
-        ))}
-      </ul>
-    );
-  };
 
   return (
     <div>
@@ -85,18 +94,14 @@ const DatasetPage = () => {
       <div className="min-h-screen bg-background text-primary_text p-8">
         <div className="bg-background p-8 max-w-6xl mx-auto">
           <div className="flex justify-between">
-            <h1 className="text-4xl font-extrabold text-primary_text mb-4">
-              {`Dataset #${dataset.id + 1}`}
-            </h1>
+            <h1 className="text-4xl font-extrabold text-primary_text mb-4">{`Dataset #${dataset.id + 1}`}</h1>
             <div className="text-lg text-primary/80">
               <p>{formatCreationTime(dataset.creationTime)}</p>
             </div>
           </div>
 
           <div className="mt-8">
-            <h4 className="text-2xl font-semibold text-primary_text">
-              Description
-            </h4>
+            <h4 className="text-2xl font-semibold text-primary_text">Description</h4>
             {descriptionLoading ? (
               <Spinner />
             ) : (
@@ -107,9 +112,7 @@ const DatasetPage = () => {
           </div>
 
           <div className="mt-8">
-            <h4 className="text-2xl font-semibold text-primary_text">
-              Preview
-            </h4>
+            <h4 className="text-2xl font-semibold text-primary_text">Preview</h4>
             {csvLoading ? (
               <Spinner />
             ) : csvData.length > 0 ? (
@@ -138,30 +141,40 @@ const DatasetPage = () => {
                 </table>
               </div>
             ) : (
-              <p className="text-primary/60">
-                No preview available for this dataset.
-              </p>
+              <p className="text-primary/60">No preview available for this dataset.</p>
             )}
           </div>
 
           <div className="mt-6">
-            <h4 className="text-2xl font-semibold text-primary_text">Tags</h4>
-            {dataset.tags.length > 0 ? (
-              <div className="flex flex-wrap mt-2">
-                {dataset.tags.map((tag: string, index: number) => (
-                  <span
-                    key={index}
-                    className="bg-primary/10 text-primary_text/70 px-3 py-1 rounded-lg mr-3 mb-3 text-base"
-                  >
-                    {tag}
-                  </span>
-                ))}
-              </div>
-            ) : (
-              <p className="text-primary/60">
-                No tags available for this dataset.
-              </p>
-            )}
+            <div className="flex items-center">
+              <h4 className="text-2xl font-semibold text-primary_text">Ask AI</h4>
+              <FaRobot className="w-10" />
+            </div>
+
+            <div className="mt-4">
+              {responses.map((resp, index) => (
+                <div key={index} className="p-4 mb-2 border rounded-lg bg-background text-primary_text">
+                  <h5 className="font-semibold">AI Response {index + 1}:</h5>
+                  <p>{resp}</p>
+                </div>
+              ))}
+            </div>
+
+            <form onSubmit={handleQuerySubmit} className="mt-4">
+              <input
+                autoFocus
+                id="askAI"
+                type="text"
+                value={userQuery}
+                onChange={(e) => setUserQuery(e.target.value)}
+                className="w-full p-2 border rounded-lg"
+                placeholder="Write your query about the dataset here."
+                required
+              />
+              <button type="submit" className="mt-2 p-2 bg-primary text-white rounded-lg">
+                {queryLoading ? "Asking..." : "Ask"}
+              </button>
+            </form>
           </div>
         </div>
       </div>
